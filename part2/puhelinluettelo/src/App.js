0982527846206261b2894535eb995d48eab1ccb0
Teemu Lindgren <43570846+Tenxa@users.noreply.github.com>
2020-05-21 00:25:32 +0300

    2.8: puhelinluettelo step3 done

--- part2/puhelinluettelo/src/App.js
+++ part2/puhelinluettelo/src/App.js
@@ -4,10 +4,13 @@ const App = () => {
   const [persons, setPersons] = useState([
     {
       name: 'Arto Hellas',
+      number: '040-123456',
       id: 1
     }
   ])
+
   const [newName, setNewName] = useState('')
+  const [newNumber, setNewNumber] = useState('')
 
   const addPerson = (event) => {
     event.preventDefault()
@@ -23,6 +26,7 @@ const App = () => {
     if (boolFlag === false) {
       const personObj = {
         name: newName,
+        number: newNumber,
         id: persons.length + 1
       }
 
@@ -39,22 +43,22 @@ const App = () => {
     setNewName(event.target.value)
   }
 
+  const handleNumberChange = (event) => {
+    setNewNumber(event.target.value)
+  }
 
   //console.log(persons)
   return (
     <div>
       <h2>Phonebook</h2>
       <form onSubmit={addPerson}>
-        <div>
-          name: <input value={newName} onChange={handleNameChange} />
-        </div>
-        <div>
-          <button type="submit">add</button>
-        </div>
+        <div>name: <input value={newName} onChange={handleNameChange} /></div>
+        <div>number: <input value={newNumber} onChange={handleNumberChange} /></div>
+        <div><button type="submit">add</button></div>
       </form>
       <h2>Numbers</h2>
-      {persons.map((person, i) =>
-        <div key={i}>{person.name}</div>
+      {persons.map((person) =>
+        <div key={person.id}>{person.name} {person.number}</div>
       )}
 
     </div>
