acce38ddd28402b53e9d5f538db7cd02c9d71234
Teemu Lindgren <43570846+Tenxa@users.noreply.github.com>
2020-05-20 13:47:04 +0300

    2.2: kurssitiedot step7 done

--- part2/kurssitiedot/src/components/Course.js
+++ part2/kurssitiedot/src/components/Course.js
@@ -4,9 +4,19 @@ import Content from './Content'
 const Course = ({ course }) => {
     return (
         <div>
-            <Content course={course}/>
+            <Content course={course} />
+            <b>Total of {Sum(course)} excerices</b>
         </div>
     )
 }
+
+const Sum = (props) => {
+    const total = props.parts.reduce((prev, next) =>
+        prev + next.exercises, 0
+    )
+
+    return (total)
+}
+
 
 export default Course
