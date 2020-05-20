f5035bd4b2a53076cba2e086ecc0ca9cf971c0a2
Teemu Lindgren <43570846+Tenxa@users.noreply.github.com>
2020-05-21 00:17:57 +0300

    2.7: puhelinluettelo step2 done

--- part2/puhelinluettelo/src/App.js
+++ part2/puhelinluettelo/src/App.js
@@ -2,27 +2,45 @@ import React, { useState } from 'react'
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas',
-      id: 1 }
+    {
+      name: 'Arto Hellas',
+      id: 1
+    }
   ])
   const [newName, setNewName] = useState('')
 
   const addPerson = (event) => {
     event.preventDefault()
-    const personObj = {
-      name: newName,
-      id: persons.length +1
+
+    var boolFlag = false
+    persons.forEach(person => {
+      if (person.name === newName) {
+        window.alert(`${newName} is already added to phonebook`)
+        boolFlag = true
+      }
+    })
+
+    if (boolFlag === false) {
+      const personObj = {
+        name: newName,
+        id: persons.length + 1
+      }
+
+      setPersons(persons.concat(personObj))
+      setNewName('')
+      console.log(boolFlag)
     }
 
-    setPersons(persons.concat(personObj))
-    setNewName('')
   }
+
+
 
   const handleNameChange = (event) => {
     setNewName(event.target.value)
   }
 
-  console.log(persons)
+
+  //console.log(persons)
   return (
     <div>
       <h2>Phonebook</h2>
