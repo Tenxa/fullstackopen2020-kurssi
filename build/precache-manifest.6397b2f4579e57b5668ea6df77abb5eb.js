f7eb29c35286050556cf2191867f0effd0bc947b
Teemu Lindgren <43570846+Tenxa@users.noreply.github.com>
2020-09-09 11:44:55 +0300

    Heroku deployment

---
+++ build/precache-manifest.6397b2f4579e57b5668ea6df77abb5eb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "030b41adb99ba28d6c12ee8b971f4d6f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7567e08bd603a1ba62f6",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "c9a736b30335e4a7f81c",
+    "url": "/static/js/2.42f56bda.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.42f56bda.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7567e08bd603a1ba62f6",
+    "url": "/static/js/main.412a43b2.chunk.js"
+  },
+  {
+    "revision": "24aecb121d5a1e31ca85",
+    "url": "/static/js/runtime-main.455ce434.js"
+  }
+]);
