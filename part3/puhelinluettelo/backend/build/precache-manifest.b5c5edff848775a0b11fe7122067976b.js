6a1dd994c6194787d897e559db6282b9b8187d24
Teemu Lindgren <43570846+Tenxa@users.noreply.github.com>
2020-10-14 21:50:38 +0300

    uibuild

---
+++ part3/puhelinluettelo/backend/build/precache-manifest.b5c5edff848775a0b11fe7122067976b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1290851c850226ca65bc77d6b2b1353b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "505efbba067e8d7994d2",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "c9a736b30335e4a7f81c",
+    "url": "/static/js/2.42f56bda.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.42f56bda.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "505efbba067e8d7994d2",
+    "url": "/static/js/main.9c11bdad.chunk.js"
+  },
+  {
+    "revision": "24aecb121d5a1e31ca85",
+    "url": "/static/js/runtime-main.455ce434.js"
+  }
+]);
