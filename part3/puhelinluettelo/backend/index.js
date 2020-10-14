10abcbe4dec8b033de1f78c0ab6cd68d75eceada
Teemu Lindgren <43570846+Tenxa@users.noreply.github.com>
2020-10-14 14:45:46 +0300

    3.16: puhelinluettelo ja tietokanta, step4. Done

--- part3/puhelinluettelo/backend/index.js
+++ part3/puhelinluettelo/backend/index.js
@@ -39,7 +39,7 @@ app.get('/api/persons/:id', (request, response, next) => {
             }
         })
         .catch(error => next(error))
-        
+
 })
 
 app.delete('/api/persons/:id', (request, response, next) => {
@@ -70,6 +70,26 @@ app.post('/api/persons', (request, response) => {
         response.json(savedPerson)
     })
 })
+
+//Error handling
+const unknownEndpoint = (request, response) => {
+    response.status(404).send({ error: 'unknown endpoint' })
+}
+
+app.use(unknownEndpoint)
+
+const errorHandler = (error, request, response, next) => {
+    console.error(error.message)
+
+    if (error.name === 'CastError' && error.kind == 'ObjectId') {
+        return response.status(400).send({ error: 'malformatted id' })
+    }
+
+    next(error)
+}
+
+app.use(errorHandler)
+
 
 const PORT = process.env.PORT
 app.listen(PORT, () => {
