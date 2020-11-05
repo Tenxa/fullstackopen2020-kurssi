aac3f16954af44f7cfdcc4959d948a20128a9f02
Teemu Lindgren <43570846+Tenxa@users.noreply.github.com>
2020-11-05 14:51:08 +0200

    fix

--- part3/puhelinluettelo/frontend/src/components/Filter.js
+++
@@ -1,5 +0,0 @@
-import React from 'react'
-
-const Filter = ({ filterText, handleChangeFilterText }) => <div><form>Filter: <input value={filterText} onChange={handleChangeFilterText} /></form></div>
-
-export default Filter
