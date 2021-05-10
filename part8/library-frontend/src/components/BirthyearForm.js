af1b0dd55e4894b414d6ea32b1090d5fcc0fa4da
Tenxa <teemu51lindgren@gmail.com>
2021-05-10 13:35:13 +0300

    8.8-8.12

---
+++ part8/library-frontend/src/components/BirthyearForm.js
@@ -0,0 +1,71 @@
+import { useMutation } from '@apollo/client'
+import React, { useEffect, useState } from 'react'
+import { SET_BIRTHYEAR, ALL_AUTHORS } from '../queries'
+import Select from 'react-select'
+
+const BirthyearForm = ({ authors }) => {
+  const [name, setName] = useState('')
+  const [born, setBorn] = useState('')
+  const [selectedOption, setSelectedOption] = useState(null)
+
+  const [setBirthyear, result] = useMutation(SET_BIRTHYEAR, {
+    refetchQueries: [{ query: ALL_AUTHORS }],
+    onError: (error) => {
+      console.log(error.graphQLErrors[0].message)
+    }
+  })
+
+  useEffect(() => {
+    if (result.data && result.data.editAuthor === null) {
+      console.log('Erroria')
+    }
+  }, [result.data])
+
+  const submit = (event) => {
+    event.preventDefault()
+
+    setBirthyear({ variables: { name, setBornTo: born * 1 } })
+
+    setName('')
+    setBorn('')
+  }
+
+const options = authors.map(author => ({ value: author.name, label: author.name }))
+
+
+  return (
+    <div>
+      <h3>Set birthyear</h3>
+
+      <form onSubmit={submit}>
+        {/* select tag toteutus
+        <div>
+          name
+          <select value={name} onChange={({ target }) => setName(target.value)}>
+            {authors.map((author, i) => 
+              <option key={i} value={author.name}>{author.name}</option>
+            )}
+          </select>
+        </div>
+        */}
+        <div>
+          <Select 
+            defaultValue={selectedOption}
+            onChange={setSelectedOption}
+            options={options}
+          />
+        </div>
+        <div>
+          born
+          <input
+            value={born}
+            onChange={({ target }) => setBorn(target.value)}
+          />
+        </div>
+        <button type='submit'>update author</button>
+      </form>
+    </div>
+  )
+}
+
+export default BirthyearForm
